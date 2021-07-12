c9e246f09a55485327ee7e18a4f0faac8ae31048
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-07-12 23:15:07 +0300

    Summary

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Blog(models.Model):
+
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    description = models.TextField(verbose_name='Описание')
+    date = models.DateField()
+    
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        #verbose_name = "Фильм"
+        #verbose_name_plural = "Фильмы"
+        ordering = ['title']
--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from portfolio.views import all_blogs
+from blog.views import all_blogs, detail
 
+
+app_name = 'blog'
 
 urlpatterns = [
     path('', all_blogs, name='all_blogs'),
+    path('<int:blog_id>/', detail, name='detail'),
 ]
