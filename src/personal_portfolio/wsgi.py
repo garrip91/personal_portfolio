39fbf5d5b35340004ba18c6f330dc5fd042c6f19
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-12-08 20:22:01 +0300

    Summary

--- src/personal_portfolio/wsgi.py
+++ src/personal_portfolio/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = os.path.expanduser('~/django3-personal-portfolio/src/personal_portfolio/settings/')
+if path not in sys.path:
+    sys.path.insert(0, path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_portfolio.settings')
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
